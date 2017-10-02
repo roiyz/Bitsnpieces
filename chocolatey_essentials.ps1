c8ad9b40a84f048f69e9b81232231a53366e6273
roiyz <roiy.zysman@gmail.com>
2017-10-02 15:32:39 -0700

    Essential apps for a windows desktop (chocolatey)

---
+++ chocolatey_essentials.ps1
@@ -0,0 +1,12 @@
+﻿iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+$env:Path += ";%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install git  -y
+choco install notepadplusplus.install -y
+choco install GoogleChrome -y 
+choco install 7zip.install -y
+choco install fiddler  -y
+choco install jmeter -y
+choco install paint.net -y
+choco install beyondcompare -y
+choco install office365proplus -y
+choco install onedrive -y
